edc837dd5b25ca61b174c1044a96c17b7f3e7a65
lucaspelloni2 <lucas_pelloni@hotmail.com>
2017-12-02 16:26:38 +0100

    set configuration manager

---
+++ src/ConfigurationManager/ConfigurationManager.java
@@ -0,0 +1,103 @@
+package ConfigurationManager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.lang.reflect.Method;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * This class represents the connection between the project and the <config.properties> file
+ *
+ * @author Lucas Pelloni
+ * @version 1.0
+ * @since 02.12.2017
+ */
+public class ConfigurationManager {
+
+    private static final String STATIC_CONTENT = "---------STATIC+CONTENT";
+    private static ConfigurationManager instance;
+    private static String filename;
+    private static String content;
+
+    static {
+        setFilename("config.properties");
+    }
+
+    private String myFilename;
+    private Properties properties;
+
+    private ConfigurationManager(String filename) throws IOException {
+        this.properties = new Properties();
+        this.properties.load(new FileInputStream(filename));
+        this.myFilename = filename;
+    }
+
+    private ConfigurationManager() {
+        this.myFilename = "";
+    }
+
+    /**
+     * Sets the filename of the configuration file. To be called before <code>getInstance</code>.
+     *
+     * @return
+     */
+    public static void setFilename(String pFilename) {
+        filename = pFilename;
+        content = null;
+    }
+
+    public static void setDirectContent(String pContent) {
+        content = pContent;
+        filename = STATIC_CONTENT + UUID.randomUUID();
+    }
+
+    /**
+     * Returns an instance of the configuration manager. Call <code>setFilename</code> before this method.
+     *
+     * @return the instance of <ConfigurationManager>
+     */
+    public static ConfigurationManager getInstance() {
+//        instance = new ConfigurationManager();
+        try {
+            if (instance == null || !instance.myFilename.equals(filename)) {
+                if (!filename.startsWith(STATIC_CONTENT))
+                    instance = new ConfigurationManager(filename);
+                else {
+                    instance = new ConfigurationManager();
+                    instance.directLoadContent(filename, content);
+                }
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            System.err.println("A problem occurred with the config Manager");
+        }
+        return instance;
+    }
+
+    public Method[] getMethods() {
+        return this.getClass().getMethods();
+    }
+
+    private void directLoadContent(String pFilename, String pContent) throws IOException {
+        this.properties = new Properties();
+        this.properties.load(new StringReader(pContent));
+        this.myFilename = pFilename;
+    }
+
+
+    public String getTestFilePath() {
+        return this.properties.getProperty("test_file");
+    }
+
+    public String getENFilesPath() {
+        return this.properties.getProperty("en_files");
+    }
+
+    public String getDEFilesPath() {
+        return this.properties.getProperty("de_files");
+    }
+
+
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+/**
+ * Created by LuckyP on 02.12.17.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("ajisfijafj");
+    }
+}
