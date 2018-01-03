461184dc8ccc08cd0843d7451cc99659dce591d9
Schaufelbühl <Andreas.Schaufelbuehl@mgb.ch>
2018-01-03 13:00:25 +0100

    Extended XMLParser, so it adds the IsClaim value, also changed the class from an abstract to an Singleton, because there is no need for abstract class ==> no inheritance

---
+++ test/XMLParserTest.java
@@ -0,0 +1,35 @@
+import ConfigurationManager.ConfigurationManager;
+import InputOutput.XMLParser;
+import Main.MicroText;
+import Main.TextSegment;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class XMLParserTest {
+
+    private List<MicroText> microTexts;
+    private final String DATASET_PATH = ConfigurationManager.getInstance().getFilePath();
+
+    @Before
+    public void setup() {
+        XMLParser xmlParser = XMLParser.getInstance();
+        microTexts = xmlParser.walkXMLFiles(DATASET_PATH);
+    }
+
+    /**
+     * Tests at the first MicrosText, if the isClaim is set correctly
+     */
+    @Test
+    public void testIsClaimSetter() {
+        List<TextSegment> textSegments = microTexts.get(0).getTextSegments();
+
+        Assert.assertFalse(textSegments.get(0).getClaim());
+        Assert.assertFalse(textSegments.get(1).getClaim());
+        Assert.assertFalse(textSegments.get(2).getClaim());
+        Assert.assertFalse(textSegments.get(3).getClaim());
+        Assert.assertTrue(textSegments.get(4).getClaim());
+    }
+}
