b0f64adeecd45a879007f625f3d57fcf701cd48f
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-01-15 18:06:34 +0100

    added shuffeling list before split

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -20,20 +20,21 @@ public class Main {
         XMLParser xmlParser = XMLParser.getInstance();
         List<MicroText> microTexts = xmlParser.walkXMLFiles(DATASET_PATH);
 
-//        StanceClassifier stanceClassifer = new StanceClassifier();
-//        stanceClassifer.run(microTexts);
 //        ProponentOponentClassifier proponentOponentClassifier = new ProponentOponentClassifier();
 //        proponentOponentClassifier.run(microTexts, 10);
+        IsClaimClassifier isClaimClassifier = new IsClaimClassifier();
+        isClaimClassifier.run(microTexts, 10);
 //        TargetClassifier targetClassifier = new TargetClassifier();
 //        targetClassifier.run(microTexts, 10);
-//        IsClaimClassifier isClaimClassifier = new IsClaimClassifier();
-//        isClaimClassifier.run(microTexts, 10);
 //        AttackSupportClassifier attackSupportClassifier = new AttackSupportClassifier();
 //        attackSupportClassifier.run(microTexts, 10);
-        RebutUndercutClassifier rebutUndercutClassifier = new RebutUndercutClassifier();
-        rebutUndercutClassifier.run(microTexts, 10);
+//        RebutUndercutClassifier rebutUndercutClassifier = new RebutUndercutClassifier();
+//        rebutUndercutClassifier.run(microTexts, 10);
     }
 }
+
+//        StanceClassifier stanceClassifer = new StanceClassifier();
+//        stanceClassifer.run(microTexts);
 
 //        FileWriter.writeToProConFolder(corpuses, DATASET_PATH);
 
